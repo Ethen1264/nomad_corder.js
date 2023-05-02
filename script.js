3c95a4109e9cfe32aa524fe1ec68dbfb4a8e8965
Ethen1264 <jxorhks86909763@gmail.com>
2023-05-02 22:51:54 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+/* const a  =5;
+const b = 2;
+let myName="nico";
+
+console.log(a+b);
+console.log(a*b);                                             // const, let
+console.log(a/b);
+console.log("hello " + myName);
+myName = "nicolas"
+
+console.log("your new name is " + myName); */
+
+
+const amIFat = null;
+let something;
+console.log(amIFat);
+console.log(something, amIFat);
