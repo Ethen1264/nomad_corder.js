1fcdb6644e28e240d20ff51d081f29bd2a6e48e0
Ethen1264 <jxorhks86909763@gmail.com>
2023-05-03 21:00:00 +0900

    second commit

--- script.js
+++ script.js
@@ -10,8 +10,42 @@ myName = "nicolas"
 
 console.log("your new name is " + myName); */
 
+// ------------------------------------------------------------------------------------------
 
-const amIFat = null;
-let something;
+/*const amIFat = null;
 console.log(amIFat);
-console.log(something, amIFat);
+console.log(something, amIFat);*/
+
+
+/*const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+let something;                                               // array
+const sat = "sat";
+const sun = "sun"; */
+
+/* const daysOfWeek = ["mon" , "tue" , "wed" , "thu" , "fri" , "sat"];
+
+const nonsense =  [1, 2, "hello", false, null, true, undefined, "nico" ];
+
+console.log(daysOfWeek, nonsense);
+
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek); */
+
+// ------------------------------------------------------------------------------------------
+
+const player = {
+  name: "nico",
+  points: 10,
+  fat: true,
+};
+console.log(player);
+player.fat = false;
+player.lastName = "potato";
+player.points = player.points+15;
+console.log(player);
+console.log(player.name);
